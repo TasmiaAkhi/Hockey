931368d79fd9c52573969b2696914311eee701ef
unknown <Tasmiaakhi>
2024-01-24 18:53:27 +0600

    Hockey first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+
+
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    colors: {" primary-color": '#FF4240',
+
+    }
+  }
+}
+
+
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'Grid-1': "url('images2.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
+  }
+}
